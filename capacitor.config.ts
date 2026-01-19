2dbd0954ce722727587047d773b5057a2f064afc
ANTHONYSAGBAY <sagbayanthony7@gmail.com>
2026-01-19 12:10:24 -0500

    build: configuración de APK con IP de red local y logo oficial

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Pastibot2.0',
+  appId: 'com.pastibot.app',
+  appName: 'Pastibot',
   webDir: 'dist'
 };
 
